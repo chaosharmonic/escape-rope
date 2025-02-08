02bb555c2fa2c5f98523cddfc7dec1e9f0dfc259
Sean Morrissey <hey@bhmt.dev>
2025-02-08 18:09:02 -0500

    More settings handling

--- src/controller/settings.ts
+++ src/controller/settings.ts
@@ -1,4 +1,8 @@
-import { Settings } from "../types.ts"
+import {
+    Blocklist,
+    CoverLetterTemplate,
+    Settings
+} from "../types.ts"
 import { db } from '../db.ts'
 
 export const getSettings = async () => {
@@ -8,8 +12,87 @@ export const getSettings = async () => {
 }
 
 export const updateSettings = async (settings: Settings) => {
-    const result = await db.settings.updateOne()
+    const result = await db.settings.updateOne(settings, {strategy: 'merge-shallow'})
 
     return result
 }
 
+const updatePartialSettings = async (
+    newSetting, // TODO: actually enforce the name
+    // something something subset properties
+    // look up whatever TypeScript has on this
+    campaignName: string = 'user'
+) => {
+    const { value: settings } = await db.settings.getOne()
+    
+    console.log({campaignName})
+
+    if (campaignName == 'user') {
+        return await db.settings.updateOne({ ...newSetting })
+    }
+
+    const { campaigns } = settings
+    
+    const target = campaigns
+    .findIndex(({name}) => name == campaignName)
+
+    const newValue = { ...campaigns[target], ...newSetting }
+
+    const next = campaigns.toSpliced(target, 1, newValue)
+
+    // TODO: on fixing the typing, I maybe
+    //  could just call the collection's update
+    //  handler directly
+    return await updateSettings({
+        ...settings,
+        campaigns: [ ...next ]
+    })
+}
+
+const updateSingleGlobalSetting = async (newSetting) =>
+    await updatePartialSettings({ ...newSetting })
+
+export const updateCoverLetters = async (
+    coverLetters: CoverLetterTemplate[],
+    campaignName: string = 'user'
+) => {
+    if (campaignName == 'user') {
+        return await updateSingleGlobalSetting({
+            ...coverLetters
+        })
+    }
+
+    return await updatePartialSettings({
+        ...coverLetters
+    }, campaignName)
+}
+
+export const updateBlocklist = async (
+    blocklist: Blocklist,
+    campaignName: string = 'user'
+) => {
+    if (campaignName == 'user') {
+        return await updateSingleGlobalSetting({
+            ...blocklist
+        })
+    }
+
+    return await updatePartialSettings({
+        ...blocklist
+    }, campaignName)
+}
+
+export const updateDefaultInterviewQuestions = async (
+    defaultInterviewQuestions: string[],
+    campaignName: string = 'user'
+) => {
+    if (campaignName == 'user') {
+        return await updateSingleGlobalSetting({
+            defaultInterviewQuestions
+        })
+    }
+
+    return await updatePartialSettings({
+        defaultInterviewQuestions
+    }, campaignName)
+}
--- src/routes/settings.js
+++ src/routes/settings.js
@@ -9,13 +9,59 @@ settingsRouter.get('/', async (ctx) => {
   ctx.response.body = res
 })
 
-settingsRouter.put('/', async (ctx) => {
-  const payload = await ctx.request.body().value
+settingsRouter.post('/', async (ctx) => {
+  const payload = await ctx.request.body.json()
+
+  console.log({ payload })
 
   const res = await settingsController
     .updateSettings(payload)
 
   ctx.response.body = res
 })
+  
+// TODO: route params
+// (when I get to support for multiple searches)
+// (*maybe* break these out into their own model)
+settingsRouter.put('/campaign/cover_letters', async (ctx) => {
+  // ...if I'm getting this granular
+  //  can I reasonably just go back to FormData?
+  const payload = await ctx.request.body.json()
+
+  const res = await settingsController
+    .updateCoverLetters({ ...payload }, 'default')
+
+  ctx.response.body = res
+})
+
+settingsRouter.put('/campaign/blocklist', async (ctx) => {
+  // this can *definitely* be formdata
+  const payload = await ctx.request.body.json()
+
+  const res = await settingsController
+    .updateCoverLetters({ ...payload }, 'default')
+
+  ctx.response.body = res
+})
+
+settingsRouter.put('/campaign/interview_questions', async (ctx) => {
+  const payload = await ctx.request.body.formData()
+
+  console.log({ payload })
+
+  const defaultInterviewQuestions = JSON.parse(
+    payload.get('defaultInterviewQuestions')
+  )
+
+
+  const res = await settingsController
+    .updateDefaultInterviewQuestions(
+      defaultInterviewQuestions,
+      'default'
+    )
+
+  ctx.response.body = res
+})
+
 
 export default settingsRouter
